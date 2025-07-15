d5a812a31e1dd215e52238d7150cc57b5639272d
bdbch <6538827+bdbch@users.noreply.github.com>
2025-07-15 23:10:26 +0200

    fix broken types for extend function chaining (#6651)

--- packages/core/src/Node.ts
+++ packages/core/src/Node.ts
@@ -367,4 +367,29 @@ export class Node<Options = any, Storage = any> extends Extendable<Options, Stor
   }
 
   // 🔥 移除通用的 moni 属性增强，改为在各个具体节点中明确定义
+
+  configure(options?: Partial<Options>) {
+    return super.configure(options) as Node<Options, Storage>
+  }
+
+  extend<
+    ExtendedOptions = Options,
+    ExtendedStorage = Storage,
+    ExtendedConfig = NodeConfig<ExtendedOptions, ExtendedStorage>,
+  >(
+    extendedConfig?:
+      | (() => Partial<ExtendedConfig>)
+      | (Partial<ExtendedConfig> &
+          ThisType<{
+            name: string
+            options: ExtendedOptions
+            storage: ExtendedStorage
+            editor: Editor
+            type: NodeType
+          }>),
+  ): Node<ExtendedOptions, ExtendedStorage> {
+    // If the extended config is a function, execute it to get the configuration object
+    const resolvedConfig = typeof extendedConfig === 'function' ? extendedConfig() : extendedConfig
+    return super.extend(resolvedConfig) as Node<ExtendedOptions, ExtendedStorage>
+  }
 }
