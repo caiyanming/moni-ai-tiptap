d58f49aedf1ba73a8c13f66634f212df12c0ec60
bdbch <6538827+bdbch@users.noreply.github.com>
2025-07-15 13:41:41 +0200

    fix global react overwrites via jsx-runtime (#6639)

--- packages/extension-blockquote/src/blockquote.tsx
+++ packages/extension-blockquote/src/blockquote.tsx
@@ -1,6 +1,5 @@
 /** @jsxImportSource @tiptap/core */
 import { mergeAttributes, Node, wrappingInputRule } from '@tiptap/core'
-import type { DOMOutputSpecArray } from '@tiptap/core/jsx-runtime'
 
 export interface BlockquoteOptions {
   /**
@@ -164,7 +163,7 @@ export const Blockquote = Node.create<BlockquoteOptions>({
       <blockquote {...mergeAttributes(this.options.HTMLAttributes, HTMLAttributes)}>
         <slot />
       </blockquote>
-    ) as unknown as DOMOutputSpecArray
+    )
   },
 
   addCommands() {
